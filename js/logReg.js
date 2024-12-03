59a32908ad6c4347e1dd854b7fa6e544793aac74
MohamedWage <154805714+MohamedWage@users.noreply.github.com>
2024-12-03 17:07:25 +0200

    Add files via upload

--- js/logReg.js
+++ js/logReg.js
@@ -2,14 +2,14 @@ var log = document.querySelector(".login")
 var reg = document.querySelector(".reg")
 var goReg = document.querySelector(".login p a")
 var goLog = document.querySelector(".reg p a")
-goReg.addEventListener("click", function(){
+goReg.addEventListener("click", function () {
     log.style.display = "none"
     reg.style.display = "block"
 })
-goLog.addEventListener("click", function goLogin(){
+goLog.addEventListener("click", function goLogin() {
     log.style.display = "block"
     reg.style.display = "none"
-    
+
 })
 
 // violotion in register
@@ -18,35 +18,43 @@ var userName = document.querySelector("#fName")
 var pass1 = document.querySelector("#pass1")
 var pass2 = document.querySelector("#pass2")
 var email = document.querySelector(".reg #email")
-subReg.addEventListener("click" , function(){
-    console.log(pass1.value)
-        console.log(pass2.value)
-        console.log(userName.value)
-        console.log(email.value)
-    if((userName.value == "")|| 
-    (pass1.value=="")||
-    (pass2.value=="")|| 
-    (email.value == "")){
+var lname = document.querySelector("#lname")
+subReg.addEventListener("click", function () {
+    const Gender = document.querySelector("input[name='gender']:checked");
+    // console.log(genderType)
+    // console.log(!Gender)
+    // console.log(pass1.value.length)
+    if ((userName.value == "") ||
+        (pass1.value == "") ||
+        (pass2.value == "") ||
+        (email.value == "") ||
+        (lname.value == "") ||
+        (!Gender)) {
         alert("please fill data")
         return;
     }
-   
-    
-    if(email.value.includes("@")){
-        
-        if(pass1.value == pass2.value){
-            localStorage.setItem("pass" , JSON.stringify(pass1.value))
-            localStorage.setItem("userName", JSON.stringify(userName.value))
-            localStorage.setItem("email", JSON.stringify(email.value))
-            log.style.display = "block"
-            reg.style.display = "none"
-            
+
+
+    if (email.value.includes("@")) {
+
+        if (pass1.value == pass2.value) {
+            if (pass1.value.length > 7) {
+                localStorage.setItem("pass", JSON.stringify(pass1.value))
+                localStorage.setItem("userName", JSON.stringify(userName.value))
+                localStorage.setItem("email", JSON.stringify(email.value))
+                log.style.display = "block"
+                reg.style.display = "none"
+                window.location = 'index.html'
+            }
+            else {
+                alert("the password must be more than 7")
+            }
         }
-        else{
+        else {
             alert("The password does not match")
         }
     }
-    else{
+    else {
         alert("please enter a email correct")
     }
 
@@ -58,20 +66,18 @@ var emaillog = document.querySelector(".login #email")
 var pass = document.querySelector("#pass")
 var sublog = document.querySelector(".login button")
 
-sublog.addEventListener("click",function (){
-    if(
-        pass.value == ""||
-        emaillog.value == ""
-    ){
+sublog.addEventListener("click", function () {
+    if (
+        pass.value == "" ||
+        emaillog.value == "") {
         alert("please fill data")
     }
-    else{
-        if(pass.value == JSON.parse(localStorage.getItem("pass"))&&
-        emaillog.value == JSON.parse(localStorage.getItem("email"))
-        ){
-             window.location = "index.html"
+    else {
+        if (pass.value == JSON.parse(localStorage.getItem("pass")) &&
+            emaillog.value == JSON.parse(localStorage.getItem("email"))) {
+            window.location = "index.html"
         }
-        else{
+        else {
             alert("password or email is wrong")
         }
     }
